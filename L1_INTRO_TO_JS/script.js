772c79153dae3a344043d289006b2a3da6e467d4
Sarthak Mishra <sarthakjm2284@gmail.com>
2025-07-01 15:38:24 +0530

    Lecture1 Introduction To JS

---
+++ L1_INTRO_TO_JS/script.js
@@ -0,0 +1,73 @@
+// // //  console.log("Hello World");
+
+
+// // // var a = 123;
+
+// // // console.log(a);
+
+// // // function abc(){
+// // //     console.log("This is function abc...");
+// // //     function hgf(){
+// // //         console.log("This is HGF......");
+// // //     }
+// // //     hgf();
+// // // }
+
+// // // abc();
+
+// // // let b = 300;
+// // // console.log(b);
+
+// // // {
+// // //     let x = 100;
+// // //     let y = 200;
+// // //     let z = 300;
+// // //     {
+// // //         console.log(x);
+// // //         console.log(y);
+// // //         console.log(z);
+        
+// // //         console.log(aa);
+// // //         {
+// // //             let aa = 20000;
+// // //             console.log(x);
+// // //             console.log(y);
+// // //             console.log(z);
+// // //         }
+// // //     }
+// // // }
+
+
+// // function abc(){
+// //     let a = 100;
+// //     let b = 200;
+// //     let c = 300;
+// //     console.log(d);
+// //     function def(){
+// //         let d = 400;
+// //         function ijk(){
+// //             console.log(a);
+// //             console.log(b);
+// //             console.log(c);
+// //         }
+// //         ijk()
+// //     }
+// //     def()
+// // }
+// // abc()
+
+// function abc(){
+//     var d = 4000;
+// }
+// abc();
+
+// console.log(d);
+
+for (var i = 0; i<=5; i++){
+   function closure(i){
+     setTimeout(()=>{
+        console.log(i);
+    },2000)
+}
+closure(i);
+}
---
+++ L1_INTRO_TO_JS/tempCodeRunnerFile.js
@@ -0,0 +1,16 @@
+ console.log("Hello World");
+
+
+// var a = 123;
+
+// console.log(a);
+
+// function abc(){
+//     console.log("This is function abc...");
+//     function hgf(){
+//         console.log("This is HGF......");
+//     }
+//     hgf();
+// }
+
+// abc();
