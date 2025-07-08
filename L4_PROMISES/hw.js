0261302621062b636ef00f311a8172a3780bddea
Sarthak Mishra <sarthakjm2284@gmail.com>
2025-07-08 15:22:16 +0530

    Changes in Project

--- L4_PROMISES/hw.js
+++ L4_PROMISES/hw.js
@@ -7,9 +7,7 @@ async function getData(){
         list.innerHTML = ""
         data.products.forEach(product => {
             const li = document.createElement("li");
-            li.textContent = `${product.title} - Rs${Math.round(product.price*85)}- ${product.category} - ${product.rating}
-           <br>
-                <img src="${product.thumbnail}" alt="${product.title}" width="100">`;
+            li.textContent = `${product.title} - Rs${Math.round(product.price*85)}`
             list.appendChild(li);
         });
     }
@@ -18,4 +16,28 @@ async function getData(){
     }
 }
 
+async function getDataTable(){
+    try{
+    const response = await fetch('https://dummyjson.com/products')
+    if(!response.ok) throw new Error('Cannot fetch Products');
+    const data = await response.json();
+    const tbody = document.querySelector("#productsTable tbody")
+    tbody.innerHTML = "";
+    data.products.forEach(product=>{
+        const row = document.createElement("tr");
+        row.innerHTML = `
+            <td>${product.id}</td>
+            <td>${product.title}</td>
+            <td>${product.description}</td>
+            <td><img src="${product.thumbnail}" alt="${product.title}" width="100" /></td>
+            <td>${Math.floor(product.price*85.74)}</td>
+        `;
+        tbody.appendChild(row);
+    })
+}
+catch(error){
+    console.error("Error: " + error.message);
+}
+}
 getData();
+getDataTable();
